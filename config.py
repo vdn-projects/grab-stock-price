065c33c437bacfdfb8a76d0e3172d1d27736bef7
Nguyen Van Duc <vanduc.dn@gmail.com>
2019-04-30 11:03:33 +0700

    Revise the path in python

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 db_username = "vietnamstock_usr1"
 db_psw = "stock@2019"
-local_test = True
+local_test = False
 conn_string = f"host=127.0.0.1 dbname=vietnam_stock user={db_username} password={db_psw}"
-download_path = os.getcwd() + "/data/download"
-initial_load_path = os.getcwd() + "/data/initial_load"
+download_path = "./data/download"  # os.getcwd()
+initial_load_path = "./data/initial_load"
